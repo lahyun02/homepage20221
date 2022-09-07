54007aa9191104037bcf1e18cf89b099c18afb3b
lahyun02 <bethankful07@naver.com>
2022-09-07 18:15:16 +0900

    2022년 9월 7일
    네이버api 로그인

--- homepage/src/main/java/egovframework/let/login/web/LoginController.java
+++ homepage/src/main/java/egovframework/let/login/web/LoginController.java
@@ -1,20 +1,24 @@
 package egovframework.let.login.web;
-import java.util.List;
-import java.util.Map;
-
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.context.request.RequestAttributes;
-import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.github.scribejava.core.model.OAuth2AccessToken;
 
 import egovframework.com.cmm.EgovMessageSource;
 import egovframework.com.cmm.LoginVO;
+import egovframework.let.api.naver.service.NaverLoginService;
+import egovframework.let.join.service.JoinService;
+import egovframework.let.join.service.JoinVO;
 import egovframework.let.uat.uia.service.EgovLoginService;
 
 
@@ -27,6 +31,12 @@ public class LoginController {
 	
 	@Resource(name = "egovMessageSource")
 	EgovMessageSource egovMessageSource;
+	
+	@Resource(name = "naverLoginService")
+	private NaverLoginService naverLoginService;
+	
+	@Resource(name = "joinService")
+	private JoinService joinService;
 	
 	//로그인
 	@RequestMapping(value = "/login/actionLogin.do")
@@ -56,6 +66,49 @@ public class LoginController {
 		request.getSession().invalidate();  //사용자가 여태까지 했던 행위에 대한 모든 세션을 삭제. 
 		
 		return "forward:/index.do"; 
+	}
+	
+	//네이버 로그인 콜백
+	@RequestMapping(value = "/login/naverLogin.do")
+	public String naverLogin(@ModelAttribute("loginVO") LoginVO loginVO, @RequestParam String code, @RequestParam String state, HttpSession session, HttpServletRequest request, HttpServletResponse response, ModelMap model) throws Exception {
+		//@RequestParam 파라미터 값을 받아오기 위해.  파라미터 값이 없으면 바로 에러메시지. 
+		
+		String domain = request.getServerName();
+		OAuth2AccessToken oauthToken;
+		oauthToken = naverLoginService.getAccessToken(session, code, state, domain);
+		
+		//로그인 사용자 정보를 읽어온다. 
+		String apiResult = naverLoginService.getUserProfile(oauthToken);
+		
+		JSONParser parser = new JSONParser();
+		Object obj = parser.parse(apiResult);	//스트링으로 받아온 값을 json으로 변환
+		JSONObject jsonObj = (JSONObject) obj;		//org.json.simple.JSONObject -> json-simple 라이브러리 사용
+		JSONObject result = (JSONObject) jsonObj.get("response");
+		
+		loginVO.setId("NAVER-" + result.get("id").toString());
+		loginVO.setPassword("");
+		loginVO.setUserSe("USR");
+		
+		LoginVO resultVO = loginService.actionLogin(loginVO);
+		//로그인 값이 없으면 회원가입처리 //네이버 -> 로그인했을때 바로 회원가입까지
+		
+		if (resultVO != null && resultVO.getId() != null & !("").equals(resultVO.getId())){
+			request.getSession().setAttribute("LoginVO", resultVO);
+			return "forward:/index.do";
+		} else {
+			//일반가입을 제외하고는 ID값은 SNS명 + ID값
+			JoinVO joinVO = new JoinVO();
+			joinVO.setEmplyrId(loginVO.getId());
+			joinVO.setUserNm(result.get("name").toString());
+			joinVO.setPassword("");
+			joinVO.setPasswordHint("SNS가입자");
+			joinVO.setPasswordCnsr("SNS가입자"); 
+			
+			joinService.insertJoin(joinVO);
+			model.addAttribute("loginMessage", "회원가입이 완료되었습니다.");
+			
+			return "forward:/index.do";
+		}
 	}
 	
 	
