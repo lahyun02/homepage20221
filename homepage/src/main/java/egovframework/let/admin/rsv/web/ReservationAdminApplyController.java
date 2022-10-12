227fdfa531ff05e5392918c57ba4896b1b303974
lahyun02 <bethankful07@naver.com>
2022-10-12 16:49:06 +0900

    2022년 10월 12일 수업내용
    관리자 예약자 RUD
    사용자 예약자 RUD

---
+++ homepage/src/main/java/egovframework/let/admin/rsv/web/ReservationAdminApplyController.java
@@ -0,0 +1,132 @@
+package egovframework.let.admin.rsv.web;
+import java.util.List;
+import java.util.Map;
+
+import egovframework.com.cmm.ComDefaultCodeVO;
+import egovframework.com.cmm.EgovMessageSource;
+import egovframework.com.cmm.LoginVO;
+import egovframework.com.cmm.service.EgovCmmUseService;
+import egovframework.com.cmm.service.EgovFileMngService;
+import egovframework.com.cmm.service.EgovFileMngUtil;
+import egovframework.com.cmm.service.FileVO;
+import egovframework.com.cmm.util.EgovUserDetailsHelper;
+import egovframework.let.rsv.service.ReservationApplyService;
+import egovframework.let.rsv.service.ReservationApplyVO;
+import egovframework.let.rsv.service.ReservationService;
+import egovframework.let.rsv.service.ReservationVO;
+import egovframework.let.utl.fcc.service.EgovStringUtil;
+import egovframework.let.utl.fcc.service.FileMngUtil;
+import egovframework.rte.fdl.cmmn.exception.EgovBizException;
+import egovframework.rte.fdl.property.EgovPropertyService;
+import egovframework.rte.psl.dataaccess.util.EgovMap;
+import egovframework.rte.ptl.mvc.tags.ui.pagination.PaginationInfo;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springmodules.validation.commons.DefaultBeanValidator;
+
+@Controller
+public class ReservationAdminApplyController {
+	
+	@Resource(name = "reservationApplyService")
+	private ReservationApplyService reservationApplyService;
+	
+	@Resource(name = "reservationService")
+	private ReservationService reservationService;
+	
+	@Resource(name = "EgovFileMngUtil")
+	private EgovFileMngUtil fileUtil;
+	
+	//예약자정보 목록 가져오기
+	@RequestMapping(value = "/admin/rsv/selectApplyList.do")
+	public String selectApplyList(@ModelAttribute("searchVO") ReservationApplyVO searchVO, HttpServletRequest request, ModelMap model) throws Exception {
+		
+		LoginVO user = (LoginVO)EgovUserDetailsHelper.getAuthenticatedUser();
+		if(user == null || user.getId() == null) {
+			model.addAttribute("message", "로그인 후 사용가능합니다.");
+			return "redirect:/index.do";
+		}
+		
+		//관리자
+		searchVO.setMngAt("Y");
+		
+		List<EgovMap> resultList = reservationApplyService.selectReservationApplyList(searchVO);
+		model.addAttribute("resultList", resultList);
+		
+		return "admin/rsv/RsvApplySelectList";
+	}
+	
+	//예약자정보 상세
+	@RequestMapping(value = "/admin/rsv/rsvApplySelect.do")
+	public String rsvApplySelect(@ModelAttribute("searchVO") ReservationApplyVO searchVO, HttpServletRequest request, ModelMap model) throws Exception {
+		LoginVO user = (LoginVO)EgovUserDetailsHelper.getAuthenticatedUser();
+		if(user == null || user.getId() == null) {
+			model.addAttribute("message", "로그인 후 사용가능합니다.");
+			return "redirect:/index.do"; //Globals.MAIN_PAGE;
+		}else {
+			model.addAttribute("USER_INFO", user);
+		}
+		
+		ReservationApplyVO result = reservationApplyService.selectReservationApply(searchVO);
+		
+		model.addAttribute("result", result);
+		
+		request.getSession().removeAttribute("sessionReservationApply"); 
+		
+		return "admin/rsv/RsvApplySelect";
+		
+	}
+	
+	//예약정보 승인
+	@RequestMapping(value = "/admin/rsv/rsvApplyConfirm.do")
+	public String updateReservationConfirm(@ModelAttribute("searchVO") ReservationApplyVO searchVO, HttpServletRequest request, ModelMap model) throws Exception {
+		
+		//이중 서브밋 방지
+		if(request.getSession().getAttribute("sessionReservationApply") != null) {
+			return "forward:/admin/rsv/selectApplyList.do";
+		}
+		
+		LoginVO user = (LoginVO)EgovUserDetailsHelper.getAuthenticatedUser();
+		if(user == null || user.getId() == null) {
+			model.addAttribute("message", "로그인 후 사용가능합니다.");
+			return "redirect:/index.do"; // + Globals.MAIN_PAGE;
+		}
+		
+		searchVO.setUserId(user.getId());
+		
+		reservationApplyService.updateReservationConfirm(searchVO);
+		
+		//이중 서브밋 방지 
+		request.getSession().setAttribute("sessionReservationApply", searchVO); 
+		return "forward:/admin/rsv/selectApplyList.do";
+	}
+	
+	//예약정보 삭제하기
+	@RequestMapping(value = "/admin/rsv/rsvApplyDelete.do")
+	public String rsvApplyDelete(@ModelAttribute("searchVO") ReservationApplyVO searchVO, HttpServletRequest request, ModelMap model) throws Exception {
+		
+		LoginVO user = (LoginVO)EgovUserDetailsHelper.getAuthenticatedUser();
+		if(user == null || user.getId() == null) {
+			model.addAttribute("message", "로그인 후 사용가능합니다.");
+			return "forward:/index.do"; // + Globals.MAIN_PAGE;
+		}
+		
+		searchVO.setUserId(user.getId());
+		
+		reservationApplyService.deleteReservationApply(searchVO);
+		
+		return "forward:/admin/rsv/selectApplyList.do";
+	}
+	
+}
